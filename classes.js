26d7d3ceaf5adcb7c8412daeb7b48dceb9425251
Nikhil Singh <nikhilsinghg18051999@gmail.com>
2025-03-02 03:50:31 +0530

    Oops-class1

---
+++ classes.js
@@ -0,0 +1,17 @@
+class Product {
+    constructor() {
+        //constructor function to create new real life instances called as object.
+        //
+        
+
+    }
+
+
+    //member functions
+    displayProduct(){
+        
+    }
+    buyProduct(){
+        
+    }
+}
