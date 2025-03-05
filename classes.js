69351a89799f8cb9e9bb6aaed81bac46247ebc10
Nikhil Singh <nikhilsinghg18051999@gmail.com>
2025-03-06 04:43:37 +0530

    Commit for the object

--- classes.js
+++ classes.js
@@ -1,17 +1,31 @@
 class Product {
-    constructor() {
-        //constructor function to create new real life instances called as object.
-        //
-        
+    constructor(n, p) {
+        // constructor function to create new real life instances called as object.
+        // When we create an object the constructor function that gets called.
 
-    }
+        // To define data members we need to initialize them inside of the const. function.
+        // To initialize them we use the this. keyword.
+        // Data members:
+        this.name = n;
+        this.price = p;
+        this.discount = d;     
 
 
     //member functions
+    
+}
+
     displayProduct(){
-        
+        console.log(this.name,this.price,this.discount)
     }
+
     buyProduct(){
         
     }
 }
+
+let iphone = new Product("Iphone",500000);
+let Laptop = new Product("Laptop",60000);
+
+console.log(iphone);
+console.log(Laptop);
